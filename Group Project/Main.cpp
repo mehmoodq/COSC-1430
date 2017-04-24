61bb8a5172e7eac3683597bba924674d0aa4bd3c
mehmood qadri <mehmood.qadri21@gmail.com>
2017-04-24 16:28:43 -0500

    initial upload

---
+++ Group Project/Main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+char reference[27] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+
+int main() 
+
+{
+	ifstream inputstream;
+	inputstream.open("name.txt");
+
+
+	
+	
+
+
+}
