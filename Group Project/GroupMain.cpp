00df3c90505e8f9133d92dad92520b64f3fa8cd5
prakashadhikari1 <prakashadhikaridanda123@gmail.com>
2017-04-27 19:40:38 -0500

    Update GroupMain.cpp

--- Group Project/GroupMain.cpp
+++ Group Project/GroupMain.cpp
@@ -8,36 +8,4 @@ using namespace std;
 
 int main() 
 {
-int main()
-{
-    int finalTotalScore = 0;
-    
-    std::ifstream infile("names.txt");
-    std::string line;
-    int counter = 1;
-    
-    while (std::getline(infile, line))
-    {
-        int totalScore = 0;
-        int tempScore = 0;
-        std::istringstream iss(line);
-        string word;
-        
-        if (!(iss >> word)) { break; } // error
-      
-        for(int i=0;i<word.length();i++){
-            int _char = word[i];
-            tempScore += (_char-64);
-        }
-       
-        totalScore = tempScore*counter;
-        cout<<"For "<<word<<" the score is: "<<totalScore<<endl;
-        finalTotalScore += totalScore;
-        counter++;
-    }
-    cout<<"Final score of all names : "<<finalTotalScore<<endl;
-    return 0;
-}
-	
-	
-}
+
