a077eee52d0ba041c7469fc23c8ec6b32ecfe3a0
Mehmood Qadri <mehmood.qadri21@gmail.com>
2017-05-07 22:33:33 -0500

    Update GroupMain.cpp

--- Group Project/GroupMain.cpp
+++ Group Project/GroupMain.cpp
@@ -2,46 +2,37 @@
 #include <fstream>
 #include <vector>
 #include <map>
-#include <string>
 #include "nameObj.h"
-#include "nameObj.cpp"
 
 using namespace std;
 
-int main()
+int main() 
 {
-    
-    
 
-    ifstream instream;
-    ifstream myfile("names.txt");
-    
-    if (instream.fail())
-    {
-        
-        cout << "File failed to open." << endl;
-        exit(1);
-    }
-    
-    string nextLine;
-       
-    vector<nameObj> list;
-    map <nameObj, int> nameScores;
-    
-    
-    string line;
-    while(getline(myfile,line)){
-    
-    
-          if (!instream.eof()) {
-        
-        
-        
-                nameObj temp(nextLine);
-                list.push_back(temp);
-                nameScores[temp] = temp.computeScore();
-        
-        
-    }
-}
+	ifstream instream;
+	instream.open("names.txt");
+
+	if (instream.fail())
+	{
+
+		cout << "File failed to open." << endl;
+		exit(1);
+	}
+
+	string nextLine;
+	vector<nameObj> list;
+	map <nameObj, int> nameScores;
+
+	int finalScore;
+
+	while (!(instream.eof())) {
+
+		nextLine = instream.getline; //Should store next line of file into nextLine, but gives error for some reason.
+		nameObj temp(nextLine);
+		list.push_back(temp);
+		nameScores[temp] = temp.computeScore();
+
+
+	}
+
 }
